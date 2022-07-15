3ce1c394080a2d336827715eea1a588bbd92d8e1
wmorales <wmorales@kubixcorp.com>
2022-07-15 17:11:05 -0600

    Updates

--- assets/main.js
+++ assets/main.js
@@ -19,7 +19,7 @@ async function fetchData (urlApi){
 try{
 const videos = await fetchData (API);
 let view = `
-${videos.items.map(video =>`
+${videos.items.map(video=>`
 <div class="group relative">
           <div
             class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
@@ -33,11 +33,9 @@ ${videos.items.map(video =>`
           </div>
         </div>
 `).slice(0,4).join('')}
-`;
-content.innerHTML= view;
+`;content.innerHTML= view;
 }catch (error){
 console.log(error);
-alert (error);
 }
 })();
 //fetch('', options)
